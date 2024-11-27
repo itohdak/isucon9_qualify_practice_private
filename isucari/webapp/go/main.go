8209dd0fa2ea54574942e605ca96d4af0f564e1c
itohdak <itohdak@gmail.com>
2024-11-27 22:16:34 +0900

    cache all categories on init

--- isucari/webapp/go/main.go
+++ isucari/webapp/go/main.go
@@ -392,6 +392,26 @@ func getCSRFToken(r *http.Request) string {
 	return csrfToken.(string)
 }
 
+var (
+	allCategories             = []Category{}
+	categoryCacheByID         = map[int]Category{}
+	categoryIDCacheByParentID = map[int][]int{}
+)
+
+func getAllCategoryForCache() error {
+	categories := []Category{}
+	err := dbx.Select(&categories, "SELECT * FROM categories")
+	if err != nil {
+		return fmt.Errorf("failed to get categories")
+	}
+	allCategories = categories
+	for _, category := range categories {
+		categoryCacheByID[category.ID] = category
+		categoryIDCacheByParentID[category.ParentID] = append(categoryIDCacheByParentID[category.ParentID], category.ID)
+	}
+	return nil
+}
+
 func getUser(r *http.Request) (user User, errCode int, errMsg string) {
 	session := getSession(r)
 	userID, ok := session.Values["user_id"]
@@ -424,14 +444,7 @@ func getUserSimpleByID(q sqlx.Queryer, userID int64) (userSimple UserSimple, err
 }
 
 func getCategoryByID(q sqlx.Queryer, categoryID int) (category Category, err error) {
-	err = sqlx.Get(q, &category, "SELECT * FROM `categories` WHERE `id` = ?", categoryID)
-	if category.ParentID != 0 {
-		parentCategory, err := getCategoryByID(q, category.ParentID)
-		if err != nil {
-			return category, err
-		}
-		category.ParentCategoryName = parentCategory.CategoryName
-	}
+	category = categoryCacheByID[categoryID]
 	return category, err
 }
 
@@ -512,6 +525,12 @@ func postInitialize(w http.ResponseWriter, r *http.Request) {
 		Campaign: 0,
 		// 実装言語を返す
 		Language: "Go",
+	}
+
+	if err = getAllCategoryForCache(); err != nil {
+		log.Print(err)
+		outputErrorMsg(w, http.StatusInternalServerError, "db error")
+		return
 	}
 
 	w.Header().Set("Content-Type", "application/json;charset=utf-8")
@@ -635,12 +654,7 @@ func getNewCategoryItems(w http.ResponseWriter, r *http.Request) {
 	}
 
 	var categoryIDs []int
-	err = dbx.Select(&categoryIDs, "SELECT id FROM `categories` WHERE parent_id=?", rootCategory.ID)
-	if err != nil {
-		log.Print(err)
-		outputErrorMsg(w, http.StatusInternalServerError, "db error")
-		return
-	}
+	categoryIDs = categoryIDCacheByParentID[rootCategory.ID]
 
 	query := r.URL.Query()
 	itemIDStr := query.Get("item_id")
@@ -2174,14 +2188,7 @@ func getSettings(w http.ResponseWriter, r *http.Request) {
 
 	ress.PaymentServiceURL = getPaymentServiceURL()
 
-	categories := []Category{}
-
-	err := dbx.Select(&categories, "SELECT * FROM `categories`")
-	if err != nil {
-		log.Print(err)
-		outputErrorMsg(w, http.StatusInternalServerError, "db error")
-		return
-	}
+	categories := allCategories
 	ress.Categories = categories
 
 	w.Header().Set("Content-Type", "application/json;charset=utf-8")
